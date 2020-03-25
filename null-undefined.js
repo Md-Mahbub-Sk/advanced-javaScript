01b41cacb0d932d786880c3b2d7b4cf85edbdd98
Md-Mahbub-Sk <skmahbubkr786@gmail.com>
2020-03-25 19:14:39 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,7 @@
+var x = null;
+console.log(x);
+console.log(typeof(x));
+// null is an object . The value of a null and undefined is same . but their type are not same.
+var y = undefined;
+console.log(x==y);
+// যদি কোনো কিছু নাল করে রাখা হয় , তার মানে এখানে পরবর্তীতে কিছু একটা সেট করা হবে। আর আন্ডিফাইন্ড মানে কোনো ভ্যালুই সেট করা হয়নি।
