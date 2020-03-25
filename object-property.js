954a940a01286a64012e96e9ff009705fe9c0d5b
Md-Mahbub-Sk <skmahbubkr786@gmail.com>
2020-03-25 22:38:51 +0600

    onek kichu janlam

--- object-property.js
+++ object-property.js
@@ -14,10 +14,19 @@ console.log(name);
 
 // another way to get names and ids.. this is possible by map .
 
+
+
 var students  = student.map(sNames => sNames.name); // function sName (sName){sName.name};
 var studentsIds = student.map(sIds =>sIds.id);
+var studentsRoll = student.map(sRoll =>sRoll.roll);
 console.log(studentsIds);
+console.log(studentsRoll);
 console.log(students);
+
+
+
+
+
 var bigger = student.filter(function idsBigger (idsBigger){
     var idsBiggerThan = idsBigger.id>40;
     return idsBiggerThan;
@@ -25,6 +34,8 @@ var bigger = student.filter(function idsBigger (idsBigger){
 // console.log(bigger);
  bigger = student.filter(sIds =>sIds.id>40);
  console.log(bigger);
+
+
  biggerOne = student.find(sIds =>sIds.id>40);// find means get first one element .. filter and find are not same they have two    different, Number 1 is filter get all elements when its condition is true or find get just start element just get one.. this is very simple.. don't use bangla because it is not readable ...
  console.log(biggerOne);
 
