eb24209c702c368c967641d095fc87533171c92d
Md-Mahbub-Sk <skmahbubkr786@gmail.com>
2020-03-25 19:32:03 +0600

    double equal vs triple equal almost completed

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,18 @@
+const x = 2;
+const y = "2";
+if (x==y){
+    console.log("this condition is true");
+}
+else{
+    console.log("this condition is false");
+}
+var xx = 2; 
+var yy = "2";
+if(xx===yy){
+    console.log("this condition is true");
+}
+else{
+    console.log("this condition is false");
+}
+// double equal means ,are both value same or not? or triple equal means are both value same and their type same or not....
+// ডাবল সমান মানে উভয়ের ভ্যালু সমান কিনা!! আর ত্রিপল সমান মানে তাদের ভ্যালু টাইপ সমান কিনা!!...
--- null-undefined.js
+++ null-undefined.js
@@ -4,4 +4,5 @@ console.log(typeof(x));
 // null is an object . The value of a null and undefined is same . but their type are not same.
 var y = undefined;
 console.log(x==y);
-// যদি কোনো কিছু নাল করে রাখা হয় , তার মানে এখানে পরবর্তীতে কিছু একটা সেট করা হবে। আর আন্ডিফাইন্ড মানে কোনো ভ্যালুই সেট করা হয়নি।
+// যদি কোনো কিছুর ভ্যালু নাল করে রাখা হয় , তার মানে এখানে পরবর্তীতে কিছু একটা সেট করা হবে। আর আন্ডিফাইন্ড মানে কোনো ভ্যালুই সেট করা হয়নি।
+// 
